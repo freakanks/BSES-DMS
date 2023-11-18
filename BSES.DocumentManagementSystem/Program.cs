c845eb7ab4169bd0a1f08a6e0b1ca7659d7ce751
Ankur Gaba <agaba@ie3innovations.com>
2023-11-18 14:29:25 +0530

    Added the Swagger endpoints.

--- BSES.DocumentManagementSystem/Program.cs
+++ BSES.DocumentManagementSystem/Program.cs
@@ -8,6 +8,18 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
+//Add Swagger Services 
+builder.Services.AddSwaggerGen(options =>
+{
+    options.UseInlineDefinitionsForEnums();
+    options.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo()
+    {
+        Title = "DMS API",
+        Description = "This is the DMS application which will serve as a common Document Storage point for all the internal applications by the BSES team.",
+        Version = "v1"
+    });
+});
+
 ///DMS Services Registration.
 builder.Services.AddDataServicesFileSystem()
                 .AddDataServicesDB()
@@ -22,5 +34,15 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+app.UseSwagger(options =>
+{
+    options.RouteTemplate = "DocumentManagementSystemAPI/swagger/{documentname}/swagger.json";
+});
+app.UseSwaggerUI(options =>
+{
+
+    options.SwaggerEndpoint("/DocumentManagementSystemAPI/swagger/v1/swagger.json", "DocumentManagementSystem API V1");
+    options.RoutePrefix = "DocumentManagementSystemAPI/swagger";
+});
 
 app.Run();
